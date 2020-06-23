00f457f9e62a7f2019dacaaeaebc57b4fbd9e0ee
Jiang <1873645345@qq.com>
2020-06-23 18:15:49 +0800

    lesson8

--- 第一课/config.js
+++ 第一课/config.js
@@ -1,4 +1,12 @@
 let config= {
-	jash:'ashgb'
+	image_url: 'https://shop.bwg.art/',
+	// #ifdef APP-PLUS
+	api_url: 'https://shop.bwg.art/',
+	// #endif
+	// #ifndef APP-PLUS
+	api_url: '12',
+	// #endif
+	domain: 'shop.bwg.art'
 }
+
 export default config
--- 第一课/pages/list/list.vue
+++ 第一课/pages/list/list.vue
@@ -1,16 +1,26 @@
 <template>
 	<mescroll-body ref="mescrollRef" @init="mescrollInit" :down="downOption" @down="downCallback" @up="upCallback">
-		<view class="notice">本Demo的下拉刷新: 添加新数据到列表顶部</view>
-		<view class="news-li" v-for="news in dataList" :key="news.id">
-			<view>{{news.title}}</view>
-			<view class="new-content">{{news.content}}</view>
+		<!-- <view class="notice">本Demo的下拉刷新: 添加新数据到列表顶部</view> -->
+		<view class="content">
+			<view class="newslist" v-for="(item ,index) in dataList" :key="index">
+				<image class="image" :src="image_url+item.pic" mode="aspectFill" />
+				<view class="title">{{item.name}}</view>
+				<view class="price">￥：{{item.price}}</view>
+			</view>
 		</view>
 	</mescroll-body>
 </template>
 
 <script>
 	import MescrollMixin from "@/components/mescroll-uni/mescroll-mixins.js";
-	import {apiNewList} from "@/api/mock.js"
+	
+	var _self;
+	// #ifdef APP-PLUS
+	const websiteUrl = 'https://shop.bwg.art';
+	// #endif
+	// #ifndef APP-PLUS
+	const websiteUrl = '';
+	// #endif
 	
 	export default {
 		mixins: [MescrollMixin], // 使用mixin (在main.js注册全局组件)
@@ -19,48 +29,51 @@
 				downOption: {
 					auto: false //是否在初始化后,自动执行downCallback; 默认true
 				},
+				image_url: '',
 				dataList: []
 			}
+		},
+		onLoad(e) {
+			_self = this;
+			_self.image_url = this.config.image_url;
+			console.log(this.config.api_url);
 		},
 		methods: {
 			/*下拉刷新的回调 */
 			downCallback() {
 				//联网加载数据
-				apiNewList().then(data => {
-					//联网成功的回调,隐藏下拉刷新的状态
-					this.mescroll.endSuccess();
-					//设置列表数据
-					this.dataList.unshift(data[0]);
-				}).catch(()=>{
-					//联网失败的回调,隐藏下拉刷新的状态
-					this.mescroll.endErr();
-				})
+				setTimeout(function() {
+					console.log('数据获取成功')
+					// this.dataList.unshift(data[0]);
+					// this.mescroll.endSuccess();
+				},1000);
+			
 			},
 			/*上拉加载的回调: 其中page.num:当前页 从1开始, page.size:每页数据条数,默认10 */
 			upCallback(page) {
-				//联网加载数据
-				apiNewList(page.num, page.size).then(curPageData=>{
-					//联网成功的回调,隐藏下拉刷新和上拉加载的状态;
-					//mescroll会根据传的参数,自动判断列表如果无任何数据,则提示空;列表无下一页数据,则提示无更多数据;
-					
-					//方法一(推荐): 后台接口有返回列表的总页数 totalPage
-					//this.mescroll.endByPage(curPageData.length, totalPage); //必传参数(当前页的数据个数, 总页数)
-					
-					//方法二(推荐): 后台接口有返回列表的总数据量 totalSize
-					//this.mescroll.endBySize(curPageData.length, totalSize); //必传参数(当前页的数据个数, 总数据量)
-					
-					//方法三(推荐): 您有其他方式知道是否有下一页 hasNext
-					//this.mescroll.endSuccess(curPageData.length, hasNext); //必传参数(当前页的数据个数, 是否有下一页true/false)
-					
-					//方法四 (不推荐),会存在一个小问题:比如列表共有20条数据,每页加载10条,共2页.如果只根据当前页的数据个数判断,则需翻到第三页才会知道无更多数据.
-					this.mescroll.endSuccess(curPageData.length);
-					
-					//设置列表数据
-					this.dataList=this.dataList.concat(curPageData);
-				}).catch(()=>{
-					//联网失败, 结束加载
-					this.mescroll.endErr();
-				})
+				this.getnewsList(page.num, page.size);
+//				this.mescroll.endBySize(curPageData.length, totalSize); //必传参数(当前页的数据个数, 总数据量)
+				
+			},
+			getnewsList(page,perpage) { //第一次回去数据
+				if(!page) { page = 1 }
+				if(!perpage) { perpage = 10 }
+				uni.request({
+					url: websiteUrl + '/api/diyike/goodsList',
+					method: 'POST',
+					withCredentials: true,
+					data: {
+						page: page,
+						perpage: perpage
+					},
+					success: (res) => {
+						this.dataList=this.dataList.concat(res.data.data);
+						this.mescroll.endSuccess(res.data.data.length);
+					},
+					fail: (msg) => {
+						console.log(msg)
+					}
+				});
 			}
 		}
 	}
@@ -74,16 +87,40 @@
 		border-bottom: 1upx solid #eee;
 		text-align: center;
 	}
-	/*展示上拉加载的数据列表*/
-	.news-li{
-		font-size: 32upx;
-		padding: 32upx;
-		border-bottom: 1upx solid #eee;
+	
+	.content {
+		display: flex;
+		/* flex-direction: column; */
+		flex-wrap: wrap;
+		/* align-items: center; */
+		/* justify-content: center; */
 	}
-	.news-li .new-content{
-		font-size: 28upx;
-		margin-top: 10upx;
-		margin-left: 20upx;
-		color: #666;
+	
+	.newslist {
+		width: 360rpx;
+		font-size: 30rpx;
+		margin-top: 10rpx;
+		margin-left: 10rpx;
+	}
+	
+	.title {
+		/* height: 80rpx; */
+		/* overflow: hidden;
+		white-space: nowrap;
+		text-overflow: ellipsis; */
+		width: 100%;
+		display: -webkit-box;
+		-webkit-box-orient: vertical;
+		-webkit-line-clamp: 2;
+		overflow: hidden;
+	}
+	
+	.price {
+		color: red;
+	}
+	
+	.image {
+		width: 360rpx;
+		height: 360rpx;
 	}
 </style>
